107a8b1dbb745ca5c88626c77913695338c70c8e
Charles Hals <hals.charles@gmail.com>
2023-11-21 22:30:02 +0100

    Daily Business

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import Navigation from './components/Navigation';
 
 const App = () => {
   return (
     <div>
-      HELLO ALL
-      test autocommitsssss
+      <Navigation />
+      <section className='landing-area'>
+        <h1>CHARLES HALS</h1>
+        <h2>Data engineering, process automation, software engineering
+        </h2>
+      </section>
+      <section className='expertise-area'>
+        <div className="expertise-container">
+          <div className="expertise-title">Data Engineering</div>
+          <div className="expertise-description">Description Data Engineering</div>
+        </div>
+      </section>
+      <section className="project-area">
+        <div className="project-container">
+          PROJECT 1
+        </div>
+        <div className="project-container">
+          PROJECT 2
+        </div>
+        <div className="project-container">
+          PROJECT 3
+        </div>
+      </section>
+      <section className="experience-area">
+        <div className="experience-container">
+          Experience 1
+        </div>
+      </section>
     </div>
   );
 };
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navigation = () => {
+  return <div>NAVBAR</div>;
+};
+
+export default Navigation;
